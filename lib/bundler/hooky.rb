5963b63750165fd533782da99f994112bae7a530
Alex Rocha <alex.rocha@shopify.com>
2024-11-04 13:10:35 -0800

    WIP - add after install all hook

--- lib/bundler/hooky.rb
+++ lib/bundler/hooky.rb
@@ -2,7 +2,16 @@ require "bundler/hooky/version"
 
 module Bundler
   module Hooky
-    class Error < StandardError; end
-    # Your code goes here...
+    class << self
+      def register
+
+        Bundler::Plugin.add_hook("after-install-all") do |installed_gems|
+
+          raise 'xyz'
+          $stdout.puts("Running tapioca gem")
+          system("bundle exec tapioca gem", execption: true)
+        end
+      end
+    end
   end
 end
---
+++ plugins.rb
@@ -0,0 +1,4 @@
+require "bundler/hooky/version"
+require_relative "lib/bundler/hooky"
+
+Bundler::Hooky.register
